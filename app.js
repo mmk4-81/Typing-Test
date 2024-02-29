b9cb5ed6324648b8dcb91e16eb8065ee278a5af4
mmk <mmkkarbalaei@gmail.com>
2024-02-29 22:50:42 +0330

    declaring & assigning values

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 //random quotes api
 const quoteApiUrl = "httpd://api.quotable.io/random?minLength=80&maxLength=100";
 
+const quoteSection = document.getElementById('quote');
+const userInput = document.getElementById('quote-input');
+let quote = "";
+let time = 60;
+let timer ="";
+let mistakes = "";
